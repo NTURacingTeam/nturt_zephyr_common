fa8762ad1ce0c589ceffb114825e7c4b86d06852
quantumspawner <jet22854111@gmail.com>
2025-07-13 11:55:24 +0800

    boards: remove unused boards

--- boards/nturt/dashboard/board.cmake
+++
@@ -1,6 +0,0 @@
-board_runner_args(jlink "--device=STM32H562RG" "--speed=4000")
-board_runner_args(pyocd "--target=stm32h562rgtx")
-
-# include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/nturt/rearbox/board.cmake
+++
@@ -1,6 +0,0 @@
-board_runner_args(jlink "--device=STM32H562VG" "--speed=4000")
-board_runner_args(pyocd "--target=stm32h562vgtx")
-
-# include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
