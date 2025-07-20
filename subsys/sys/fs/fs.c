6b233bcd91bddec9cefebf09a71a6e911c0df39b
quantumspawner <jet22854111@gmail.com>
2025-07-20 17:28:00 +0800

    subsys: sys: fs: remote automount and use fstab instead

--- subsys/sys/fs/fs.c
+++ subsys/sys/fs/fs.c
@@ -16,35 +16,7 @@
 #include <zephyr/fs/littlefs.h>
 #endif
 
-// project includes
-#include "nturt/sys/sys.h"
-
 LOG_MODULE_REGISTER(nturt_fs, CONFIG_NTURT_SYS_LOG_LEVEL);
-
-/* static function declaration -----------------------------------------------*/
-/// @brief Initialization function for NTURT file system module.
-static int init();
-
-/* static varaible -----------------------------------------------------------*/
-#ifdef CONFIG_NTURT_FS_FAT_FS
-static FATFS fatfs;
-static struct fs_mount_t fs_mnt = {
-    .type = FS_FATFS,
-    .fs_data = &fatfs,
-    .mnt_point = CONFIG_NTURT_FS_MOUNT_POINT,
-};
-#elif defined(CONFIG_NTURT_FS_LITTLE_FS)
-static struct fs_littlefs lfsfs;
-static struct fs_mount_t fs_mnt = {
-    .type = FS_LITTLEFS,
-    .fs_data = &lfsfs,
-    .flags = FS_MOUNT_FLAG_USE_DISK_ACCESS,
-    .storage_dev = CONFIG_NTURT_FS_SD_DISK_NAME,
-    .mnt_point = CONFIG_NTURT_FS_MOUNT_POINT,
-};
-#endif
-
-SYS_INIT(init, APPLICATION, CONFIG_NTURT_SYS_INIT_PRIORITY);
 
 /* function definition -------------------------------------------------------*/
 #ifdef CONFIG_NTURT_FS_FAT_FS_HAS_RTC
@@ -61,24 +33,6 @@ uint32_t get_fattime() {
          (tm.tm_sec / 2);
 }
 #endif
-
-/* static function definition ------------------------------------------------*/
-static int init() {
-  int ret;
-
-  ret = fs_mount(&fs_mnt);
-  if (ret < 0) {
-    LOG_ERR("Fail to mount %s: %s", CONFIG_NTURT_FS_SD_DISK_NAME,
-            strerror(-ret));
-    return ret;
-
-  } else {
-    LOG_INF("Mounted %s at %s", CONFIG_NTURT_FS_SD_DISK_NAME,
-            CONFIG_NTURT_FS_MOUNT_POINT);
-  }
-
-  return 0;
-}
 
 #ifdef CONFIG_SETTINGS
 
