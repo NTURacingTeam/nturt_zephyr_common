e427bc88f4c92b5b054e1d3d2e7b2a21cc10306f
quantumspawner <jet22854111@gmail.com>
2025-08-14 14:08:25 +0800

    subsys: msg: logging: use mpsc to buffer and sys workqueue to write to files

--- subsys/msg/logging.c
+++ subsys/msg/logging.c
@@ -1,37 +1,87 @@
 #include "nturt/msg/logging.h"
 
 // glibc includes
+#include <stdalign.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
 // zephyr includes
 #include <zephyr/fs/fs.h>
+#include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/__assert.h>
+#include <zephyr/sys/cbprintf.h>
+#include <zephyr/sys/mpsc_pbuf.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/zbus/zbus.h>
 
 // project includes
 #include "nturt/msg/msg.h"
 #include "nturt/sys/fs.h"
+#include "nturt/sys/sys.h"
 
 LOG_MODULE_REGISTER(nturt_msg_logging, CONFIG_NTURT_MSG_LOG_LEVEL);
 
+/* macro ---------------------------------------------------------------------*/
+// Use the same alignment as cbprintf as it is used to print the message.
+#define MSG_ALIGNMENT CBPRINTF_PACKAGE_ALIGNMENT
+
+#define MSG_PADDING                                                   \
+  ((sizeof(struct msg_logging_packet_header) % MSG_ALIGNMENT) > 0     \
+       ? (MSG_ALIGNMENT -                                             \
+          (sizeof(struct msg_logging_packet_header) % MSG_ALIGNMENT)) \
+       : 0)
+
+/* type ----------------------------------------------------------------------*/
+struct msg_logging_packet_header {
+  MPSC_PBUF_HDR;
+  uint32_t len : 32 - MPSC_PBUF_HDR_BITS;
+  const struct zbus_channel *chan;
+};
+
+struct msg_logging_packet {
+  struct msg_logging_packet_header header;
+  uint8_t __padding[MSG_PADDING];
+  uint8_t data[];
+};
+
+struct msg_logging_ctx {
+  struct k_work logging_work;
+
+  struct mpsc_pbuf_buffer mpsc_pbuf;
+
+  // a word is assumed to be 4 bytes by mpsc_pbuf
+  uint32_t __mpsc_pbuf_buf[CONFIG_NTURT_MSG_LOGGING_BUF_SIZE / 4];
+};
+
 /* static function declaration -----------------------------------------------*/
-static void thread(void *arg1, void *arg2, void *arg3);
+static uint32_t msg_logging_packet_get_wlen(
+    const union mpsc_pbuf_generic *packet);
+static void msg_logging_packet_drop(const struct mpsc_pbuf_buffer *buffer,
+                                    const union mpsc_pbuf_generic *packet);
+static void msg_logging_init(struct msg_logging_ctx *ctx);
+
+static int init();
+
+static void msg_cb(const struct zbus_channel *chan);
+static void logging_work(struct k_work *work);
 
 /* static variable -----------------------------------------------------------*/
-K_THREAD_DEFINE(msg_logging_thread, CONFIG_NTURT_MSG_LOGGING_THREAD_STACK_SIZE,
-                thread, NULL, NULL, NULL,
-                CONFIG_NTURT_MSG_LOGGING_THREAD_PRIORITY, 0, 0);
+static struct msg_logging_ctx g_ctx = {
+    .logging_work = Z_WORK_INITIALIZER(logging_work),
+};
 
-ZBUS_MSG_SUBSCRIBER_DEFINE(msg_logging_msg_subscriber);
+SYS_INIT(init, APPLICATION, CONFIG_NTURT_MSG_INIT_PRIORITY);
+
+ZBUS_LISTENER_DEFINE(msg_logging_listener, msg_cb);
 
 /* function definition -------------------------------------------------------*/
 int msg_chan_logging_start(const struct zbus_channel *chan, const char *file) {
-  __ASSERT(is_msg_chan(chan), "chan must be a message channel");
-  __ASSERT(strlen(file) < MAX_PATH_LEN, "file path too long");
+  __ASSERT(msg_chan_is_from_msg(chan), "chan must be a message channel");
+  __ASSERT(strlen(file) < FS_MAX_PATH_LEN, "file path too long");
 
   struct msg_chan_data *chan_data = zbus_chan_user_data(chan);
   struct msg_logging *logging = &chan_data->logging;
@@ -48,14 +98,14 @@ int msg_chan_logging_start(const struct zbus_channel *chan, const char *file) {
   fs_file_t_init(&logging->file);
   ret = fs_open(&logging->file, file, FS_O_WRITE | FS_O_APPEND);
   if (ret == -ENOENT) {
-    char dirname[MAX_PATH_LEN];
+    char dirname[FS_MAX_PATH_LEN];
     size_t len = strrchr(file, '/') - file;
     strncpy(dirname, file, len);
     dirname[len] = '\0';
 
-    ret = mkdir_p(dirname);
+    ret = fs_mkdir_p(dirname);
     if (ret < 0) {
-      LOG_ERR("mkdir_p(%s) failed: %s", dirname, strerror(-ret));
+      LOG_ERR("fs_mkdir_p(%s) failed: %s", dirname, strerror(-ret));
       goto out;
     }
 
@@ -73,7 +123,9 @@ int msg_chan_logging_start(const struct zbus_channel *chan, const char *file) {
     goto out;
   }
 
-  ret = zbus_chan_add_obs(chan, &msg_logging_msg_subscriber, K_FOREVER);
+  fs_sync(&logging->file);
+
+  ret = zbus_chan_add_obs(chan, &msg_logging_listener, K_FOREVER);
   if (ret < 0) {
     LOG_ERR("zbus_chan_add_obs(%s) failed: %s", zbus_chan_name(chan),
             strerror(-ret));
@@ -93,7 +145,7 @@ out:
 }
 
 int msg_chan_logging_stop(const struct zbus_channel *chan) {
-  __ASSERT(is_msg_chan(chan), "chan must be a message channel");
+  __ASSERT(msg_chan_is_from_msg(chan), "chan must be a message channel");
 
   struct msg_chan_data *chan_data = zbus_chan_user_data(chan);
   struct msg_logging *logging = &chan_data->logging;
@@ -107,7 +159,7 @@ int msg_chan_logging_stop(const struct zbus_channel *chan) {
     goto out;
   }
 
-  ret = zbus_chan_rm_obs(chan, &msg_logging_msg_subscriber, K_FOREVER);
+  ret = zbus_chan_rm_obs(chan, &msg_logging_listener, K_FOREVER);
   if (ret < 0) {
     LOG_ERR("zbus_chan_remove_obs(%s) failed: %s", zbus_chan_name(chan),
             strerror(-ret));
@@ -128,25 +180,98 @@ out:
 }
 
 /* static function definition ------------------------------------------------*/
-static void thread(void *arg1, void *arg2, void *arg3) {
-  const struct zbus_channel *chan;
-  uint8_t msg[CONFIG_NTURT_MSG_MAX_SIZE];
+static uint32_t msg_logging_packet_get_wlen(
+    const union mpsc_pbuf_generic *_packet) {
+  const struct msg_logging_packet *packet =
+      (const struct msg_logging_packet *)_packet;
+
+  return packet->header.len;
+}
+
+static void msg_logging_packet_drop(const struct mpsc_pbuf_buffer *buffer,
+                                    const union mpsc_pbuf_generic *_packet) {
+  (void)buffer;
+
+  const struct msg_logging_packet *packet =
+      (const struct msg_logging_packet *)_packet;
+
+  LOG_ERR("Dropping packet from %s", zbus_chan_name(packet->header.chan));
+}
+
+static void msg_logging_init(struct msg_logging_ctx *ctx) {
+  struct mpsc_pbuf_buffer_config config = {
+      .buf = ctx->__mpsc_pbuf_buf,
+      .size = ARRAY_SIZE(ctx->__mpsc_pbuf_buf),
+      .get_wlen = msg_logging_packet_get_wlen,
+      .notify_drop = msg_logging_packet_drop,
+      .flags = MPSC_PBUF_MODE_OVERWRITE,
+  };
+
+  mpsc_pbuf_init(&ctx->mpsc_pbuf, &config);
+}
+
+static int init() {
+  msg_logging_init(&g_ctx);
+
+  return 0;
+}
+
+static void msg_cb(const struct zbus_channel *chan) {
+  size_t len = sizeof(struct msg_logging_packet) + zbus_chan_msg_size(chan);
+  len = DIV_ROUND_UP(len, 4);
+
+  union mpsc_pbuf_generic *_packet =
+      mpsc_pbuf_alloc(&g_ctx.mpsc_pbuf, len, K_NO_WAIT);
+  if (_packet == NULL) {
+    LOG_ERR("Failed to allocate packet for %s", zbus_chan_name(chan));
+    return;
+  }
+
+  struct msg_logging_packet *packet = (struct msg_logging_packet *)_packet;
+
+  packet->header.len = len;
+  packet->header.chan = chan;
+  memcpy(packet->data, chan->message, zbus_chan_msg_size(chan));
+
+  mpsc_pbuf_commit(&g_ctx.mpsc_pbuf, _packet);
+
+  if (!k_work_is_pending(&g_ctx.logging_work)) {
+    sys_work_submit(&g_ctx.logging_work);
+  }
+}
+
+static void logging_work(struct k_work *work) {
+  struct msg_logging_ctx *ctx =
+      CONTAINER_OF(work, struct msg_logging_ctx, logging_work);
+
+  const union mpsc_pbuf_generic *_packet = mpsc_pbuf_claim(&ctx->mpsc_pbuf);
+  if (_packet == NULL) {
+    return;
+  }
+
+  const struct msg_logging_packet *packet =
+      (const struct msg_logging_packet *)_packet;
+
+  const struct zbus_channel *chan = packet->header.chan;
+  struct msg_chan_data *chan_data = zbus_chan_user_data(chan);
+  struct msg_logging *logging = &chan_data->logging;
+
   char buf[512];
   buf[0] = '\n';
 
-  while (
-      !zbus_sub_wait_msg(&msg_logging_msg_subscriber, &chan, msg, K_FOREVER)) {
-    struct msg_chan_data *chan_data = zbus_chan_user_data(chan);
-    struct msg_logging *logging = &chan_data->logging;
+  int size = msg_chan_csv_write(chan, packet->data, buf + 1, sizeof(buf) - 1);
+  if (size >= sizeof(buf) - 1) {
+    LOG_ERR("msg_chan_csv_write(%s) failed: buffer too small",
+            zbus_chan_name(chan));
+    return;
+  }
 
-    int size = msg_chan_csv_write(chan, msg, buf + 1, sizeof(buf) - 1);
-    if (size >= sizeof(buf) - 1) {
-      LOG_ERR("msg_chan_csv_write(%s) failed: buffer too small",
-              zbus_chan_name(chan));
-      continue;
-    }
+  fs_write(&logging->file, buf, size + 1);
+  fs_sync(&logging->file);
 
-    fs_write(&logging->file, buf, size + 1);
-    fs_sync(&logging->file);
+  mpsc_pbuf_free(&ctx->mpsc_pbuf, _packet);
+
+  if (mpsc_pbuf_is_pending(&ctx->mpsc_pbuf)) {
+    sys_work_submit(&ctx->logging_work);
   }
 }
